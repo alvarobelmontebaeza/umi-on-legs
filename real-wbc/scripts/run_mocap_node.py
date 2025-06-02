b66fa04f8ba72c2bc5d29dc8428d85910ad2a3f6
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2025-06-02 10:58:46 +0200

    rename arm

--- real-wbc/scripts/run_mocap_node.py
+++ real-wbc/scripts/run_mocap_node.py
@@ -1,4 +1,7 @@
 import rclpy
+import sys 
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from modules.mocap.mocap_node import MocapNode
 import argparse
 if __name__ == "__main__":
@@ -7,7 +10,7 @@ if __name__ == "__main__":
     parser.add_argument("--ip", type=str)
     args = parser.parse_args()
     rclpy.init()
-    agent = MocapNode(rigid_body_dict={1:"Go2Body", 2:"Arx5Gripper"}, ip=args.ip)
+    agent = MocapNode(rigid_body_dict={1:"Go2Body", 2:"WX250sGripper"}, ip=args.ip)
 
     try:
         rclpy.spin(agent)
